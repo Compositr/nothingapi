ee8fada911a80418980c945cad9cdba10d4c5166
Compositr <43405050+Compositr@users.noreply.github.com>
2022-12-13 20:14:44 +1100

    feat: add getUser() function

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export const userIdRegex = /^\d{10,}$/g;
+
+export default async function getUser(userId: string) {
+  if (!userIdRegex.test(userId))
+    throw new TypeError(
+      "Invalid userId. userId must be a valid Discord ID (Snowflake)"
+    );
+
+  const results = await fetch(
+    `https://nothingapi.crypticcode.org/users/${userId}`,
+    {
+      headers: {
+        "User-Agent": "nothingapi.js/0.0.1",
+        Accept: "application/json",
+      },
+    }
+  ).then((res) => res.json());
+
+  return {
+    userId: results.userId,
+    balance: results.balance,
+  };
+}
